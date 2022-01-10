66c5f18fce0003fb6e586489cbdb397496061472
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-10 20:57:32 +0300

    Proje dosyası ekle.

---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toplama topla = new Toplama(3,5);
+            Toplama t2 = new Toplama(topla);
+            Console.WriteLine(t2.islem());
+            topla.s1 = 2;
+            topla.s2 = 3;
+            Console.WriteLine(topla.islem());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CopyConstructor/Toplama.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    class Toplama
+    {
+        public int s1;
+        public int s2;
+        public Toplama()
+        {
+            s1 = -1;
+            s2 = -1;
+        }
+        public Toplama(int a, int b)
+        {
+            s1 = a;
+            s2 = b;
+        }
+
+        public Toplama(Toplama t)
+            {
+            s1=t.s1;
+            s2 = t.s2;
+
+            }
+
+        public int islem()
+        {
+            return s1 + s2;
+        }
+            
+}
+    }
+
